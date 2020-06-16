679646b66dbf864a0f60b1b189c3621588273d79
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-06-16 22:54:52 +0200

    Changed route

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -6,7 +6,7 @@ import './MenuBar.css';
 function MenuBar() {
     return (
         <Menu width='350px'>
-            <Link to="/" id="home" className="menu-item">Home</Link>
+            <Link to="/home" id="home" className="menu-item">Home</Link>
             <Link to="/started" id="started" className="menu-item">Ongoing</Link>
             <Link to="/recipebank" id="recipebank" className="menu-item">Recipebank</Link>
             <Link to="/flours" id="flours" className="menu-item">Flours</Link>
