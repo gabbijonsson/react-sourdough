62d9b34cfd7c1f5e399514f9ae4b9040fc17b9f4
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-06-16 22:49:43 +0200

    Updates

---
+++ precache-manifest.e59373beddbd98834f4a51d9cd1a2817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8a7cc48d2d0d45bf354f31b3a81cfa",
+    "url": "/react-sourdough/index.html"
+  },
+  {
+    "revision": "f8dacfc82bf9a97259fa",
+    "url": "/react-sourdough/static/css/main.7e2c1f24.chunk.css"
+  },
+  {
+    "revision": "18f6eed81af7ec0c9961",
+    "url": "/react-sourdough/static/js/2.51fb986a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-sourdough/static/js/2.51fb986a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dacfc82bf9a97259fa",
+    "url": "/react-sourdough/static/js/main.51dcea44.chunk.js"
+  },
+  {
+    "revision": "d7ccc86f802813ffc5d9",
+    "url": "/react-sourdough/static/js/runtime-main.74f1f10f.js"
+  },
+  {
+    "revision": "3368e71c4c16234ae3958fb7aad803d9",
+    "url": "/react-sourdough/static/media/Masonjar.3368e71c.png"
+  }
+]);
