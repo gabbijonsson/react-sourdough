a8859fa5f5ba4c6c2cf02dc7c7efc4ca6985f114
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-06-16 23:07:29 +0200

    Added switch, moved router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import LandingPage from './components/LandingPage'
-import OngoingPage from './components/OngoingPage'
-import NewBread from './components/NewBread'
-import SubmitRecipe from './components/SubmitRecipe'
-import CopyrightText from './components/CopyrightText'
-import RecipeBank from './components/RecipeBank'
-import FlourList from './components/FlourList'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import OngoingPage from "./components/OngoingPage";
+import NewBread from "./components/NewBread";
+import SubmitRecipe from "./components/SubmitRecipe";
+import CopyrightText from "./components/CopyrightText";
+import RecipeBank from "./components/RecipeBank";
+import FlourList from "./components/FlourList";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Route exact={true} path="/" component={LandingPage} />
-      <Route path="/new" component={NewBread} />
-      <Route path="/started" component={OngoingPage} />
-      <Route path="/submit" component={SubmitRecipe} />
-      <Route path="/recipebank" component={RecipeBank} />
-      <Route path="/flours" component={FlourList} />
-      <CopyrightText />
+      <Router>
+        <Switch>
+          <Route exact={true} path="/" component={LandingPage} />
+          <Route path="/new" component={NewBread} />
+          <Route path="/started" component={OngoingPage} />
+          <Route path="/submit" component={SubmitRecipe} />
+          <Route path="/recipebank" component={RecipeBank} />
+          <Route path="/flours" component={FlourList} />
+        </Switch>
+        <CopyrightText />
+      </Router>
     </div>
   );
 }
